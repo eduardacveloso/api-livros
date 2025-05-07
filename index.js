f55cfb38b1434c51ff24758f8419d4a1c2080e5d
Maria Eduarda <167576098+eduardacveloso@users.noreply.github.com>
2025-05-06 21:22:08 -0300

    Implementando a listagem de livros (#1)

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const PORT = 3000;
 
 app.use(express.json());
 
+let livros = [
+    { id: 1, titulo: "Dom Casmurro", autor: "Machado de Assis" },
+    { id: 2, titulo: "O Cortiço", autor: "Aluisio Azevedo" }
+];
+
+app.get('/livros', (req, res) => {
+    res.json(livros);
+})
+
 app.listen(PORT, () => {
     console.log(`Serviço rodando em http://localhost:${PORT}`);
 });
